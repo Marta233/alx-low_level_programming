37a330b4fa8a95d4e38848fca582a6c21ad7b3ed
Marta233 <martaassefa23@gmail.com>
2022-04-07 20:05:50 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(2, "write the correct script stated in task 8\", also some text\n", 59);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\", this is error\n", 59);
 return (1);
 }
