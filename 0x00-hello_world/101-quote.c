85be017f879f41d6a2e3c18e7d710dc9e83ee325
Marta233 <martaassefa23@gmail.com>
2022-04-07 19:54:38 +0000

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h> 
-#include <unistd.h> 
-/** 
- * main-Entry point 
- * 
- * Return: Always 0 (Success) 
- */ 
+#include <unistd.h>
+
+/**
+*
+*description comment
+*/
 int main(void)
-{ 
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
-return (1); 
+{
+write(2, "write the correct script stated in task 8\", also some text\n", 59);
+return (1);
 }
