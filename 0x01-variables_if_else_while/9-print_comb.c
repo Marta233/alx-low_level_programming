8e0f09a8adeee0c45cb7af776713e63acafd703f
Marta233 <martaassefa23@gmail.com>
2022-04-08 14:43:16 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints values of different data types
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar((n%10) + '0');
+		putchar('\n');
+		putchar(",");
+	putchar(",");
+	return (0);
+}
