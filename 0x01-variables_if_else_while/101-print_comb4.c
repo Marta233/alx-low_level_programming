f94e62c9c945ef106028cb7043a0f6d4636b53ac
Marta233 <martaassefa23@gmail.com>
2022-04-08 15:18:36 +0000

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints values of different data types
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int n1, n2 n3;
+
+	for (n1 = 0 ; n1 <= 6 ; n1++)
+	{
+		for (n2 = n1 + 1 ; n2 <= 8 ; n2++)
+		{
+			for (n3 = n2 + 1 ; n3 <= 9 ; n3++)
+			{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+			putchar((n3 % 10) + '0');
+			if (n1 == 6 && n2 == 8 && n3 ==9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
