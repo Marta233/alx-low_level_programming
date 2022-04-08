a73951d2dd8be4dd7b9f15bb674b333dd21942f2
Marta233 <martaassefa23@gmail.com>
2022-04-08 13:05:09 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point, prints values of different data types
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char Letter;
+
+		for (Letter = 'a' ; letter <= 'z' ; Letter++)
+		putchar(Letter);
+		putchar('\n');
+		return (0);
+}
