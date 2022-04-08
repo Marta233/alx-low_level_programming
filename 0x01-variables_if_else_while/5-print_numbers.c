491b7b54666e5a753853975aaae1642450fbcb99
Marta233 <martaassefa23@gmail.com>
2022-04-08 13:47:48 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints values of different data types
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 0 ; n < 10 ; n++)
+		printf("the numver is", n);
+	return (0);
+}
