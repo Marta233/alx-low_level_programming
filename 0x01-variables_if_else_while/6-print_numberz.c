fe5eb22d3c3de70c144deec8aafc51d2ec466870
Marta233 <martaassefa23@gmail.com>
2022-04-08 14:00:56 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints values of different data types
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	printf("\n");
+	return (0);
+}
