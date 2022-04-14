d4120100aa6ab05ebd442dc2c4d391f52a3e46c8
Marta233 <martaassefa23@gmail.com>
2022-04-14 21:30:52 +0300

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void positive_or_negative(int i)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
