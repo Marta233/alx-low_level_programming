66a41c6799e5a0cba0255820af8770384e678479
Marta233 <martaassefa23@gmail.com>
2022-05-03 20:02:50 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,30 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
+
 /**
- * _strdup - gives a pointer to memory space
- * @str: string
- * Return: pointer
+ * create_array - create an arry of size with char c
+ * @size: unsigned int type
+ * @c: char type
+ * Return: Return pointer to array created
  */
-char *_strdup(char *str)
+
+char *create_array(unsigned int size, char c)
 {
 	char *s;
+	unsigned int i;
 
-	unsigned int size = 0, i;
-
-	if (str == NULL)
+	if (size == 0)
+	{
 		return (NULL);
+	}
 
-	while (*(str + size))
-		size++;
-
-	s = malloc(sizeof(char) * (size + 1));
-
+	s = malloc((size) * sizeof(char));
 	if (s == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; i < size; i++)
-		*(s + i) = *(str + i);
-
-	*(s + size) = '\0';
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
 	return (s);
 }
