540b3317cf586ba86af9b489d26f950d375169fd
Marta233 <martaassefa23@gmail.com>
2022-05-09 13:59:43 +0000

    fun

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,11 +2,10 @@
 #include <dog.h>
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-	return (NULL);
-	d->name = name;
-        user->age = age;
-        user->owner = owner;
-    return user;
-
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
