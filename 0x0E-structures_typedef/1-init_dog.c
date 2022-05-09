813bbb9695f0482728f937a37c79fd40a4822a12
Marta233 <martaassefa23@gmail.com>
2022-05-09 13:58:11 +0000

    fun

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include <stflib.h>
+#include <dog.h>
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+	return (NULL);
+	d->name = name;
+        user->age = age;
+        user->owner = owner;
+    return user;
+
+}
