56c8affee1902619ead47dee2031a6e9ed261004
root <martaassefa23@gmail.com>
2022-05-24 00:40:07 +0300

    Test 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
@@ -10,7 +10,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
-	
+
 	while (h)
 	{
 		nodes++;
