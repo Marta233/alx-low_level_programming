2d3760b972ab3a58a65382f4bfba13d9dd438c5f
root <martaassefa23@gmail.com>
2022-05-23 22:45:10 +0300

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
-while (h)
-{
-	printf("print the element %d\n", h->n);
-	h = h->next;
-	nodes++;
-}
-return (nodes);
+	size_t nodes = 0;
+	
+	while (h)
+	{
+		printf("print the element %d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
 }
