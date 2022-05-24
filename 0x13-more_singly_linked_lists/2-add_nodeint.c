11248d847dc0d6991e98a819ff8624e3787ca0d4
root <martaassefa23@gmail.com>
2022-05-24 12:36:25 +0300

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		if (temp == NULL)
 			return (NULL);
 	temp->n = n;
-	temp->next = *head; 
+	temp->next = *head;
 	*head = temp;
 	return (temp);
 }
