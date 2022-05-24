fc9c83f38e56147e707505ea89c24f68a452f418
root <martaassefa23@gmail.com>
2022-05-24 13:05:59 +0300

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,11 +26,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	else 
 	{
-		temp2 = head;
+		temp2 = *head;
 		while (temp2->next != NULL)
 			temp2 = temp2->next;
 		temp2->next = temp;
 	}
 
 	return (*head);
+
 }
