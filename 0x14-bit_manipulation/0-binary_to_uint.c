b7e7e76ae0e3314fdfe1c39b4e6818db5be0fd01
root <martaassefa23@gmail.com>
2022-05-26 17:34:15 +0300

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,20 +12,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, mult = 1;
 	int len;
 
-	if (b == '\0')
-		return (0);
+	while ( num > 0)  
+    {  
+        rem = num % 10; /* divide the binary number by 10 and store the remainder in rem variable. */  
+        decimal_num = decimal_num + rem * base;  
+        num = num / 10; // divide the number with quotient  
+        base = base * 2;  
+    }
+return (decimal_num);	
+}  
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
-	}
-
-	return (num);
-}
