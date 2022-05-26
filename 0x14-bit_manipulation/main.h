ce777dc63fc30b9cbc64fcae4b81e6bf485f045d
root <martaassefa23@gmail.com>
2022-05-26 17:54:38 +0300

    Task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * AUth: Marta A. 
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
-
-int _putchar(char c);
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -17,4 +10,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* MAIN_H */
+#endif
